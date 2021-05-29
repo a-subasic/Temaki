99536436290a98994d45dae3f96775933575e51b
Bachvac <ivanaxbacac@gmail.com>
2021-05-29 18:27:56 +0200

    create project query finished

--- project.cpp
+++ project.cpp
@@ -7,17 +7,12 @@ Project::Project(QObject *parent) : QObject(parent)
 
 }
 
-//bool Project::create(const QString& name, const QList<int>& memberIds) {
-//    bool success = false;
-//    return success;
-//}
-
 QVariant Project::create(const QString& name, const QList<int>& memberIds, const int& currentUserId) {
     bool success = false;
     int project_id = 0;
 
     // Prepare for transaction
-    //QSqlDatabase::database().transaction();
+    QSqlDatabase::database().transaction();
 
     QSqlQuery projectQuery;
     projectQuery.prepare("INSERT INTO Project (name, owner_id) VALUES (:name, :owner_id)");
@@ -30,29 +25,26 @@ QVariant Project::create(const QString& name, const QList<int>& memberIds, const
         qWarning() << QString("Failed to execute 'Project.create' query. ProjectName:%1, OwnerID:%2. ERROR: %3").arg(name, QString::number(currentUserId), projectQuery.lastError().text());
     }
     else {
-        success = true;
-        qWarning() << projectQuery.next() << projectQuery.isValid() << projectQuery.lastError() << projectQuery.lastQuery() << projectQuery.ValuesAsColumns;
-        while(projectQuery.next()) {
-            project_id = projectQuery.value(0).toInt();
-            foreach(const int& memberId, memberIds){
-                QSqlQuery projectmembersQuery;
-                projectmembersQuery.prepare("INSERT INTO ProjectMembers (project_id, user_id) VALUES (:project_id, :user_id)");
-                projectmembersQuery.bindValue(":project_id", project_id);
-                projectmembersQuery.bindValue(":user_id", memberId);
+        project_id = projectQuery.lastInsertId().toInt();
+        qWarning() << "ID: " << project_id;
 
-                if(!projectmembersQuery.exec()) {
-                    qWarning() << QString("Failed to execute 'Project.create' query. Project ID:%1, Members count:%2. ERROR: %3").arg(QString::number(project_id), QString::number(memberIds.count()), projectmembersQuery.lastError().text());
-                    qWarning() << QString("Rollback transaction!");
-                    success = false;
-                    //QSqlDatabase::database().rollback(); // rollback if failed to insert ProjectMembers
-                    break;
-                }
+        foreach(const int& memberId, memberIds){
+            QSqlQuery projectmembersQuery;
+            projectmembersQuery.prepare("INSERT INTO ProjectMembers (project_id, user_id) VALUES (:project_id, :user_id)");
+            projectmembersQuery.bindValue(":project_id", project_id);
+            projectmembersQuery.bindValue(":user_id", memberId);
+
+            if(!projectmembersQuery.exec()) {
+                qWarning() << QString("Failed to execute 'Project.create' query. Project ID:%1, Members count:%2. ERROR: %3").arg(QString::number(project_id), QString::number(memberIds.count()), projectmembersQuery.lastError().text());
+                qWarning() << QString("Rollback transaction!");
+                success = false;
+                QSqlDatabase::database().rollback(); // rollback if failed to insert ProjectMembers
+                break;
             }
         }
-
     }
 
-    //QSqlDatabase::database().commit();
+    QSqlDatabase::database().commit();
     QVariantMap response;
     response.insert("success", success);
     response.insert("project_id", project_id);
@@ -62,7 +54,6 @@ QVariant Project::create(const QString& name, const QList<int>& memberIds, const
 
 QList<QVariant> Project::getAllForUser(int userId) {
     QList<QVariant> result;
-
     qInfo() << userId;
 
     QSqlQuery query;
